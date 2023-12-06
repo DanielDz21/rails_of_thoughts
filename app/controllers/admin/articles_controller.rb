be79588021beffb6b0dfe2de20e9d712e298d30e
DanielDz21 <daniel.da.silva.tj2011@gmail.com>
2023-12-06 20:03:58 -0300

    Create admin article crud

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -8,4 +8,45 @@ class Admin::ArticlesController < ApplicationController
   def show
     @article = Article.find_by(id: params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to [:admin, @article]
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to [:admin, @article]
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to admin_articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
