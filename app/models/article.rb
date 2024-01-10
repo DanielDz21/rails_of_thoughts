390c3e05cca87154e48651e4ba069ec577ce4108
DanielDz21 <daniel.da.silva.tj2011@gmail.com>
2024-01-10 07:54:36 -0300

    Add read time to article show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,10 @@ class Article < ApplicationRecord
   has_rich_text :content
 
   validates :title, presence: true
+
+  def read_time
+    words_per_minute = 220
+    content_length = content.to_plain_text.split.size
+    (content_length / words_per_minute).floor
+  end
 end
